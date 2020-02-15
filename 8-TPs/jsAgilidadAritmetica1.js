fe056a5cc4eca084f9de145c3b62817b42574d32
Marianaa <60628835+Marianc1510@users.noreply.github.com>
2020-02-15 20:22:33 -0300

    resolucion de iluminación y aritmético

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -9,12 +9,67 @@ se debe informar si el resultado es el correcto o no.
 var respuesta;
 function comenzar()
 {
+// declarar variables
+    var primerNumero;
+    var segundoNumero;
+    var operador;
+    var operadorFinal;
 
-	
-
+// definir  2 numero aleatorio del 1 al 10
+    primerNumero= Math.floor(Math.random()*(11-1)) + 1;
+    segundoNumero=Math.floor(Math.random()*(11-1)) + 1;
+// de los 2 numeros random realizar una operacion random
+    operador=Math.floor(Math.random()*(5-1)) + 1;
+     switch (operador) {
+         case 1:
+            operadorFinal= "+";
+         break;
+         case 2:
+            operadorFinal= "-";
+         break;
+         case 3:
+            operadorFinal= "*";
+         break;
+         case 4:
+            operadorFinal= "/";
+         break;
+        }
+     document.getElementById("PrimerNumero").value=primerNumero;
+     document.getElementById("SegundoNumero").value=segundoNumero;
+     document.getElementById("Operador").value=operadorFinal;
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+    var numeroUno;
+    var numeroDos;
+    var operador;
+    var respuesta;
+    var respuestaFinal;
+
+    numeroUno=document.getElementById("PrimerNumero").value;
+    numeroDos=document.getElementById("SegundoNumero").value;
+    operador=document.getElementById("Operador").value;
+    switch (operador) {
+         case "+":
+            respuestaFinal = numeroUno + numeroDos;
+         break;
+         case "-":
+            respuestaFinal = numeroUno - numeroDos;
+         break;
+         case "*":
+            respuestaFinal = numeroUno * numeroDos;
+         break;
+         case "/":
+            respuestaFinal = numeroUno / numeroDos;
+         break;
+        
+    }
+     respuesta = document.getElementById("Respuesta").value;
+     if( respuestaFinal == respuesta){
+         alert( " esta correcto ");
+        }
+        else {
+            alert( " es incorrecto ");
+        }
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,83 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+    //DECLARO VARIABLES
+     var cantidad;
+     var marca;
+     var precioUnidad;
+     var precioTotal;
+     var descuento;
+     var precioDescuento;
+     var ingresosBrutos;
+
+     //oBTENGO DATOS
+     cantidad= document.getElementById("Cantidad").value;
+     cantidad=parseInt(cantidad);
+     marca=document.getElementById("Marca").value;
+
+     //CALCULO PRECIOS
+     precioUnidad=35;
+     precioTotal= cantidad * precioUnidad;
+     
+     //SELECCION DE DESCUENTO
+     if(cantidad >=6){
+         //calcular 50
+         descuento = precioTotal * 50 /100;
+     }
+     else{
+         if(cantidad == 5 && marca=="ArgentinaLuz")
+         {  
+             //descuento 40
+            descuento = precioTotal *40 / 100;
+         }
+         else if (cantidad == 5) {
+             // descuento del 30
+            descuento = precioTotal * 30 /100;
+         }
+         else {
+
+            if( cantidad == 4 && marca=="ArgentinaLuz" || marca=="FelipeLamparas"){
+             //descuento del 25
+             descuento = precioTotal * 25 /100;
+             }
+             else if(cantidad ==4){
+             //descuento del 20
+             descuento = precioTotal *20 /100;
+            }
+            else{
+
+                if( cantidad ==3 && marca=="ArgentinaLuz") {
+                    //descuento del 15
+                    descuento = precioTotal * 15/100;
+                    }
+                else if (cantidad == 3 && marca=="FelipeLamparas"){
+                    //descuento del 10
+                    descuento = precioTotal *10 / 100;
+                    }
+                else if (cantidad == 3){
+                    //descuento del 5
+                    descuento = precioTotal * 3 / 100;
+                }
+          
+         }
+            
+        }
+
+     }
+
+     //CALCULAR VALOR FINAL UTILIZANDO EL DESCUENTO
+     precioDescuento = precioTotal - descuento;
+
+     //CHEQUEAR SI ENTRA EN INGRESOS BRUTOS
+     
+     if (precioDescuento >= 120){
+        ingresosBrutos = precioDescuento * 10 /100;
+        precioDescuento= precioDescuento + ingresosBrutos;
+        alert( " usted pago de IIBB " + ingresosBrutos);
+         }
+
+     
+     document.getElementById("precioDescuento").value= precioDescuento;
+
 }
+
